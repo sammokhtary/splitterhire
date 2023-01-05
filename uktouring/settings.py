c3522d909f332ffaa2839afa18a58e663ca39344
Sam Mokhtary <sam.mokhtary@gmail.com>
2023-01-05 23:14:21 +0000

    revoke secret key

--- uktouring/settings.py
+++ uktouring/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@6o^e&cm2na=8fg8#hgyy(916g(ij)f9^33&vgqim3a#i+-$m4'
+# Check your .env file for the secret key: https://stackoverflow.com/questions/64208678/hiding-secret-key-in-django-project-on-github-after-uploading-project
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
